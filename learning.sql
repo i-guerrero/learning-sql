b63bf11393db22641d24dae76dce30741ff23793
Isaac Guerrero <isaacguerrero@pursuit.org>
2022-08-30 16:56:03 -0400

    3rd commit

--- learning.sql
+++ learning.sql
@@ -147,12 +147,107 @@ ORDER BY o.occurred_at DESC;
 /* Use the SQL environment below to find the solution for each of the following questions. If you get stuck or want to check your answers, you can find the answers at the top of the next concept. */
 
 /* Find the total amount of poster_qty paper ordered in the orders table. */
+SELECT SUM(poster_qty) AS total_poster_sales
+FROM orders
 
 /* Find the total amount of standard_qty paper ordered in the orders table. */
+SELECT SUM(standard_qty)
+FROM orders
 
 /* Find the total dollar amount of sales using the total_amt_usd in the orders table. */
+SELECT SUM(total_amt_usd)
+FROM orders
 
 /* Find the total amount spent on standard_amt_usd and gloss_amt_usd paper for each order in the orders table. This should give a dollar amount for each order in the table. */
+SELECT SUM(standard_amt_usd) standard_sum, SUM(gloss_amt_usd) gloss_sum
+FROM orders
+
+/* Solution */
+SELECT standard_amt_usd + gloss_amt_usd AS total_standard_gloss
+FROM orders;
 
 /* Find the standard_amt_usd per unit of standard_qty paper. Your solution should use both an aggregation and a mathematical operator. */
+SELECT SUM(standard_amt_usd)/SUM(standard_qty) AS standard_price_per_unit
+FROM orders
 
+
+/* Questions: MIN, MAX, & AVERAGE */
+
+/* Use the SQL environment below to assist with answering the following questions. Whether you get stuck or you just want to double check your solutions, my answers can be found at the top of the next concept. */
+
+/* When was the earliest order ever placed? You only need to return the date. */
+SELECT MIN(occurred_at)
+FROM orders
+
+/* Try performing the same query as in question 1 without using an aggregation function. */
+SELECT occurred_at
+FROM orders
+ORDER BY occurred_at
+LIMIT 1;
+
+/* When did the most recent (latest) web_event occur? */
+SELECT MAX(occurred_at)
+FROM web_events
+
+/* Try to perform the result of the previous query without using an aggregation function. */
+SELECT occurred_at
+FROM web_events
+ORDER BY occurred_at DESC
+LIMIT 1;
+
+/* Find the mean (AVERAGE) amount spent per order on each paper type, as well as the mean amount of each paper type purchased per order. Your final answer should have 6 values - one for each paper type for the average number of sales, as well as the average amount. */
+SELECT 
+    AVG(standard_amt_usd) avg_standard_amt_usd,
+    AVG(poster_amt_usd) avg_poster_amt_usd,
+    AVG(gloss_amt_usd) avg_gloss_amt_usd,
+    AVG(standard_qty) avg_standard_qty, 
+    AVG(poster_qty) avg_poster_qty, 
+    AVG(gloss_qty) avg_gloss_qty
+FROM orders
+
+
+/* Via the video, you might be interested in how to calculate the MEDIAN. Though this is more advanced than what we have covered so far try finding - what is the MEDIAN total_usd spent on all orders? */
+SELECT *
+FROM (SELECT total_amt_usd
+      FROM orders
+      ORDER BY total_amt_usd
+      LIMIT 3457) AS Table1
+ORDER BY total_amt_usd DESC
+LIMIT 2;
+
+
+/* SQL Aggregations */
+
+/* GROUP BY Note */
+/* Now that you have been introduced to JOINs, GROUP BY, and aggregate functions, the real power of SQL starts to come to life. Try some of the below to put your skills to the test! */
+
+/* Questions: GROUP BY */
+/* Use the SQL environment below to assist with answering the following questions. Whether you get stuck or you just want to double check your solutions, my answers can be found at the top of the next concept. */
+
+/* One part that can be difficult to recognize is when it might be easiest to use an aggregate or one of the other SQL functionalities. Try some of the below to see if you can differentiate to find the easiest solution. */
+
+/* 1. Which account (by name) placed the earliest order? Your solution should have the account name and the date of the order. */
+SELECT a.name, o.occurred_at
+FROM accounts a
+JOIN orders o
+ON a.id = o.account_id
+ORDER BY o.occurred_at
+LIMIT 1;
+
+/* 2. Find the total sales in usd for each account. You should include two columns - the total sales for each company's orders in usd and the company name. */
+SELECT a.name, SUM(o.total_amt_usd)
+FROM orders o
+JOIN accounts a
+ON a.id = o.account_id
+GROUP BY a.name
+
+
+/* 3. Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event? Your query should return only three values - the date, channel, and account name. */
+
+/* 4. Find the total number of times each type of channel from the web_events was used. Your final table should have two columns - the channel and the number of times the channel was used. */
+
+/* 5. Who was the primary contact associated with the earliest web_event? */
+
+/* 6. What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account name and the total usd. Order from smallest dollar amounts to largest. */
+
+/* 7. Find the number of sales reps in each region. Your final table should have two columns - the region and the number of sales_reps. Order from fewest reps to most reps. */
